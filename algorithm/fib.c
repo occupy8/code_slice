1a64be1289c2e060692dafba9f0c0dd66190dd0b
yangkuan <yangkuan@le.com>
2017-09-22 18:48:36 +0800

    青蛙跳台阶问题

---
+++ algorithm/fib.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Fib(int n)
+{
+    if(n <= 0)
+        return 0;
+
+    if(n == 1)
+    {
+        return 1;
+    }
+    else if(n == 2)
+    {
+        return 2;
+    }
+    else
+    {
+        return Fib(n-1) + Fib(n-2);
+    }
+}
+
+int main(void){
+    int num = 5;
+
+    int max = Fib(num);
+
+    printf("num:%d, max:%d\n", num, max);
+
+    return 0;
+}
