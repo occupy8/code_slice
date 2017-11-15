1855feff28479cbcc36f65134616e375cce2c15d
yangkuan <yangkuan@le.com>
2017-11-15 17:55:00 +0800

    preorder_recursive no recursion

--- tree/preorder_recursive.cpp
+++ tree/preorder_recursive.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -18,17 +19,40 @@ void preorder_traversal(tree_node *root, vector<int> &v){
     preorder_traversal(root->right, v);
 }
 
+//非递归
+void preorder_norecursion_traversal(tree_node *root, vector<int > &v){
+	stack<tree_node *> s;
+	tree_node *p = root;
+	
+	while(p != NULL || !s.empty()){
+		while(p != NULL){
+			s.push(p);
+			v.push_back(p->val);
+			
+			p = p->left;
+		}
+		//叶子节点？？
+		if(!s.empty()){
+			p = s.top();
+			s.pop();
+
+			p = p->right;
+		}
+	}
+}
+
 int main(void){
     tree_node *root = new tree_node;
-    root->val = 3;
+    root->val = 1;
     root->left = new tree_node;
     root->right = new tree_node;
 
     root->left->val = 2;
-    root->right->val = 4;
+    root->right->val = 3;
 
-    vector<int > result;
-    preorder_traversal(root);
+    vector<int> result;
+    //preorder_traversal(root, result);
+    preorder_norecursion_traversal(root, result);
 
     for(int i = 0; i < result.size(); i++)
         cout<<result[i]<<" ";
