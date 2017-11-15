6e160af616a8bf3632b474d77566f764d6db44f9
yangkuan <yangkuan@le.com>
2017-11-15 17:42:51 +0800

    preorder tree traversal

--- tree/preorder_recursive.cpp
+++ tree/preorder_recursive.cpp
@@ -3,50 +3,37 @@
 
 using namespace std;
 
-struct TreeNode{
+struct tree_node{
     int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-
-    TreeNode(){
-        left = NULL;
-        right = NULL;
-    }
+    struct tree_node *left;
+    struct tree_node *right;
 };
 
-class Solution{
-public:
-    vector<int> vec;
-    vector<int> preorderTraversal(TreeNode *root){
-        if(root == NULL)
-            return vec;
+void preorder_traversal(tree_node *root, vector<int> &v){
+    if(NULL == root)
+        return ;
 
-        vec.push_back(root->val);
-
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
-
-        return vec;
-    }
-
-};
-
+    v.push_back(root->val); 
+    preorder_traversal(root->left, v);
+    preorder_traversal(root->right, v);
+}
 
 int main(void){
-    TreeNode *root = new TreeNode;
+    tree_node *root = new tree_node;
     root->val = 3;
-    root->left = new TreeNode;
-    root->right = new TreeNode;
+    root->left = new tree_node;
+    root->right = new tree_node;
 
     root->left->val = 2;
     root->right->val = 4;
 
-    Solution *so = new Solution;
-    vector<int > result = so->preorderTraversal(root);
+    vector<int > result;
+    preorder_traversal(root);
 
     for(int i = 0; i < result.size(); i++)
         cout<<result[i]<<" ";
 
+    cout<<endl;
     //delete
 
     return 0;
