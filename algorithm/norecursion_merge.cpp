d1b062638f6cdd139f1d6a4f1b62e04f2f4e4172
yangkuan <yangkuan@le.com>
2017-11-15 13:26:34 +0800

    norecursion_merge

--- algorithm/norecursion_merge.cpp
+++ algorithm/norecursion_merge.cpp
@@ -1,7 +1,12 @@
 /*
  * 归并排序的非递归实现
  */
-void merge(int arr[], int low, int mid, int high)
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int *arr, int low, int mid, int high)
 {
     if(arr == NULL || low >= high)
         return;
@@ -10,37 +15,62 @@ void merge(int arr[], int low, int mid, int high)
     int *ptmp = (int *)malloc(len * sizeof(int));
     if(ptmp == NULL)
        return;
-    int i0 = low;
-    int i1 = mid;
-    
-    int j0 = mid+1
-    int j1 = high;
-    
+
+    int i = low;
+    int j = mid+1;
     int t = 0;
-    while(i0 < i1 && j0 < j1){
-        while(i0 < i1 && arr[i0] < arr[j0])
-           ptmp[t++] = arr[i0++]; 
-        while(j0 < j1 && arr[j0] < arr[i0])
-            ptmp[t++] = arr[j0--];
+
+    while(i <= mid && j <= high){
+        if(arr[i] < arr[j])
+           ptmp[t++] = arr[i++]; 
+        else
+           ptmp[t++] = arr[j++];
     } 
 
-    while(i0 < i1)
-        ptmp[t++] = arr[i0++];
-    while(j0 < j1)
-        ptmp[t++] = arr[j0++];
+    while(i <= mid)
+        ptmp[t++] = arr[i++];
 
-    t = 0;
-    for(i0 = low; i0 <= high; i0++)
-        arr[i0] = ptmp[t++];
+    while(j <= high)
+        ptmp[t++] = arr[j++];
+
+    for(i = 0; i < t; i++)
+        arr[low+i] = ptmp[i];
 
     free(ptmp);
 }
 
-int main(void){
-    int arr1[] = {1, 2, 3, 8, 9, 5, 6, 10,11 };
-    int len = sizeof(arr1)/sizeof(int);
+//将a中相邻长度为s的子序列两两合并
+void merge_pass(int *arr, int s, int len){
+	if(NULL == arr || s >= len)
+		return;
+	int i = 0;
+	int j;
 
-    merge(arr1, 0, 4, len-1);
+	while(i < len - 2*s +1){
+		int mid = i+s-1;
+		int high = i+2*s-1;
+		merge(arr, i, mid, high);
+		i += 2*s;
+	}
+
+	if(i < len - s + 1)
+		merge(arr, i, i+s-1, len);
+}
+
+void merge_sort(int *arr, int len){
+	int k = 1;
+	while(k <= len){
+		merge_pass(arr, k, len);
+		k *= 2;
+	}
+}
+
+int main(void){
+    int arr1[10] = {1, 2, 3, 8, 9, 5, 6, 10,11 };
+    int len = 9;
+
+    //merge(arr1, 0, 4, len-1);
+    merge_sort(arr1, len-1);
 
     int i;
     for(i = 0; i < len; i++){
