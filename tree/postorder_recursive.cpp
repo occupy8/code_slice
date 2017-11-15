44f968e8bc7fb2c2ef2ce88506addb5f26cd6848
yangkuan <yangkuan@le.com>
2017-11-15 20:21:38 +0800

    post order no recursive traverval binary tree

--- tree/postorder_recursive.cpp
+++ tree/postorder_recursive.cpp
@@ -1,35 +1,71 @@
-void postorder_recursive(Bitree T){
-    if(T){
-        postorder_recursive(T->lchild);
-        postorder_recursive(T->rchild);
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct tree_node{
+    int val;
+    struct tree_node *left;
+    struct tree_node *right;
+};
+
+void postorder_nonrecursive(tree_node *root, vector<int> &v){
+    if(!root)
+        return;
+
+    stack<struct tree_node *> s;
+    //pcur当前访问节点，plast:上次访问节点
+    struct tree_node *pcur = root;
+    struct tree_node *plast = NULL;
+
+    //先把当前节点移动到左子树最下边
+    while(pcur){
+        s.push(pcur);
+        pcur = pcur->left;
+    }
+
+    while(!s.empty()){
+        pcur = s.top();
+        s.pop();
+        //一个根节点被访问的前提是:无右子树或右子树已经被访问过
+        if(pcur->right == NULL || pcur->right == plast){
+            v.push_back(pcur->val);
+            plast = pur;
+            /*
+             * else if(pcur->left == plast) //若左子树被访问过，则需先进入右子树（根节点需再次入栈）
+             */
+        }else{
+            //根节点再次入栈
+            s.push(pcur);
+            pcur = pcur->right;
+            while(pcur){
+                s.push(pcur);
+                pcur = pcur->left;
+            }
+        }
+
     }
 }
 
-typedef struct{
-    BTNode *ptr;
-    enum {0,1,2} mark;
-}PMType;
+int main(void){
+    tree_node *root = new tree_node;
+    root->val = 1;
+    root->left = new tree_node;
+    root->right = new tree_node;
 
-void postorder_nonrecursive(BiTree T){
-    PMType a;
-    initstack(S);  /* S的元素为PMType类型 */
-    push(S, {T,0});    /* 根结点入栈 */
-    while(!stackempty(S)){
-        pop(S, a);
-        switch(a.mark)
-        {
-            case 0:
-                push(S, {a.ptr, 1});
-                if(a.ptr->lchild)
-                    push(S, {a.ptr->lchild, 0});
-                break;
-            case 1:
-                push(S, {a.ptr,2});
-                if(a.ptr->rchild)
-                    push(S, {a.ptr->rchild, 0});
-                break;
-            case 2:
-                visit(a.ptr);
-        }
-    }
+    root->left->val = 2;
+    root->right->val = 3;
+
+    vector<int> result;
+    postorder_nonrecursive(root, result);
+    
+
+    for(int i = 0; i < result.size(); i++)
+        cout<<result[i]<<" ";
+
+    cout<<endl;
+    //delete
+
+    return 0;
 }
